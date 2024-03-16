dfeab879fdeb1269611590da11f3d399bc80ee13
Alejandro Gomez Ares <alejandro.gomezares@usp.ceu.es>
2024-03-16 11:35:10 +0000

    Codigo package info

---
+++ aplicacion/package-info.java
@@ -0,0 +1,5 @@
+//**
+  * La clase aplicacion contiene el programa principal para interactuar con la logica de la aplicacion.
+  * Es la interfaz de usuario para utilizar las funciones de la clase Matematicas.
+ */
+package aplicacion;
---
+++ mates/package-info.java
@@ -0,0 +1,4 @@
+//**
+  * La clase Matematicas contiene la logica para la aproximacion PI mediante el metodo de Montecarlo.
+  */
+package mates;
