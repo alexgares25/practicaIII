62fef5815f9af42e456648d9c4c1728a32e03add
alexgares25 <72692375+alexgares25@users.noreply.github.com>
2024-03-16 12:36:32 +0100

    Update package-info.java

--- aplicacion/package-info.java
+++ aplicacion/package-info.java
@@ -1,5 +1,5 @@
-//**
-  * La clase aplicacion contiene el programa principal para interactuar con la logica de la aplicacion.
-  * Es la interfaz de usuario para utilizar las funciones de la clase Matematicas.
+/**
+ * La clase Aplicacion contiene el programa principal para interactuar con la lógica de la aplicación.
+ * Es la interfaz de usuario para utilizar las funciones de la clase Matematicas.
  */
 package aplicacion;
