8a915c2048c7d88f63a0a509f463ece959e03aad
alexgares25 <72692375+alexgares25@users.noreply.github.com>
2024-03-18 20:51:45 +0100

    Update Matematicas.java

--- src/mates/Matematicas.java
+++ src/mates/Matematicas.java
@@ -3,14 +3,18 @@ package mates;
 import java.util.Random;
 import java.util.stream.LongStream;
 
+/**
+ * La clase Matematicas proporciona métodos para aproximación de pi utilizando el método de Montecarlo y expresiones lambda.
+ */
 public class Matematicas {
-
+    
     /**
      * Aproxima el valor de pi utilizando el método de Montecarlo y expresiones lambda.
      *
      * @param numIntentos El número de intentos o puntos generados para la aproximación.
      * @return Una aproximación del valor de pi.
      */
+    
     public static double aproximarPILambda(long numIntentos) {
         Random random = new Random();
 
