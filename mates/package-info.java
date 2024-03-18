e331daccf3363652f81829279f5376edb0ebd1ae
alexgares25 <72692375+alexgares25@users.noreply.github.com>
2024-03-18 20:04:51 +0100

    Cambuiamos package-info.java

--- mates/package-info.java
+++ mates/package-info.java
@@ -1,4 +1,6 @@
 /**
- * La clase Matematicas contiene la lógica para la aproximación de Pi mediante el método de Montecarlo.
+ * La clase Matematicas contiene la lógica para aproximar el valor de Pi mediante el método de Montecarlo utilizando expresiones lambda.
+ * Se utiliza el método de Montecarlo para generar puntos aleatorios y contar cuántos caen dentro del círculo unitario,
+ * lo que proporciona una aproximación de Pi basada en la proporción de puntos dentro del círculo.
  */
 package mates;
